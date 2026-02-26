688999237a4aacdd370fead0e8aae479b6e3dfea
erickmurigi <ericmurigi04@gmail.com>
2026-02-26 12:53:16 +0300

    Refactor code structure for improved readability and maintainability

--- controllers/verifyToken.js
+++ controllers/verifyToken.js
@@ -1,39 +1,40 @@
 import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
-export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; 
 
-    if (!token) {
-        return next(createError(401, "You are not authenticated!"));
+
+// Verifies JWT from Authorization: Bearer <token>
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(createError(401, "You are not authenticated!"));
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+
+    req.user = user; // payload
+    next();
+  });
+};
+
+// Must be logged in (no further checks)
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next);
+};
+
+// Admin only (expects payload to include isAdmin === true)
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, (err) => {
+    if (err) return next(err);
+
+    if (!req.user?.isAdmin) {
+      return next(createError(403, "You are not authorized!"));
     }
 
-    jwt.verify(token, process.env.JWT, (err, user) => {
-        if (err) return next(createError(403, "Token is not valid!"));
-        req.user = user;
-        next();
-    });
-};
-
-// Verifying normal user (unchanged logic, just uses updated verifyToken)
-export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) {
-            next();
-        } else {
-            return next(createError(403, "You are not authorized!"));
-        }
-    });
-};
-
-// Verifying admin (unchanged logic, just uses updated verifyToken)
-export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if (req.user.isAdmin) {
-            next();
-        } else {
-            return next(createError(403, "You are not admin"));
-        }
-    });
+    next();
+  });
 };
